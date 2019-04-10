db454feb3add208ccc6a6e686cdc36f3d173de66
Miklos Pavel <pavelmiklos1@gmail.com>
2019-04-10 17:21:25 +0200

    Created base project with .gitignore. Added search bar to main view controller.

---
+++ trueoverflow/trueoverflow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trueoverflow
+//
+//  Created by Mac on 2019. 04. 10..
+//  Copyright © 2019. truemotion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
